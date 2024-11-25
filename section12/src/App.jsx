1dee6ad73a7b4f57d3b02f2d3dc8a80534f6aa17
ownue <enu_i@naver.com>
2024-11-25 22:28:20 +0900

    section12

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,35 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useReducer, useRef, createContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import NotFound from "./pages/NotFound";
 
-function App() {
-  const [count, setCount] = useState(0)
+const mockData = [
+    {
+        id: 1,
+        createdDate: new Date().getTime(),
+        emotionId: 1,
+        content: "1번 일기 내용",
+    },
+    {
+        id: 2,
+        createdDate: new Date().getTime(),
+        emotionId: 2,
+        content: "2번 일기 내용",
+    },
+    {
+        id: 3,
+        createdDate: new Date().getTime(),
+        emotionId: 3,
+        content: "3번 일기 내용",
+    },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function reducer(state, action) {
+    switch (action.type) {
+        case "CREATE":
+            return [action.data, ...state];
+        case "UPDATE":
+            return state.map((item) =>
+                String(item.id) === String(action.data.id)
+                    ? action.data
+                    : item
+            );
+        case "DELETE":
+            return state.filter(
+                (item) => String(item.id) !== String(action.id)
+            );
+        default:
+            return state;
+    }
 }
 
-export default App
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
+function App() {
+    const [data, dispatch] = useReducer(reducer, mockData);
+    const idRef = useRef(3);
+
+    // 새로운 일기 추가
+    const onCreate = (createdDate, emotionId, content) => {
+        dispatch({
+            type: "CREATE",
+            date: {
+                id: idRef.current++,
+                createdDate,
+                emotionId,
+                content,
+            },
+        });
+    };
+
+    // 기존 일기 수정
+    const onUpdate = (createdDate, emotionId, content) => {
+        dispatch({
+            type: "UPDATE",
+            data: {
+                id,
+                createdDate,
+                emotionId,
+                content,
+            },
+        });
+    };
+
+    // 기존 일기 삭제
+    const onDelete = (id) => {
+        dispatch({
+            type: "DELETE",
+            id,
+        });
+    };
+
+    return (
+        <>
+            <DiaryStateContext.Provider value={data}>
+                <DiaryDispatchContext.Provider
+                    value={{
+                      onCreate,
+                      onUpdate,
+                      onDelete,
+                    }}
+                >
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/new" element={<New />} />
+                        <Route
+                            path="/diary/:id"
+                            element={<Diary />}
+                        />
+                        <Route path="/edit/:id" element={<Edit />} />
+                        <Route path="/*" element={<NotFound />} />
+                    </Routes>
+                </DiaryDispatchContext.Provider>
+            </DiaryStateContext.Provider>
+        </>
+    );
+}
+
+export default App;
